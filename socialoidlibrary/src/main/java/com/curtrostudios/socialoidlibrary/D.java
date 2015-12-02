36d38581aea4f9fe29204987d52949c049029849
power7714 <steve.curtis.jr@gmail.com>
2015-12-01 21:05:19 -0800

    update

---
+++ socialoidlibrary/src/main/java/com/curtrostudios/socialoidlibrary/D.java
@@ -0,0 +1,18 @@
+package com.curtrostudios.socialoidlibrary;
+
+@SuppressWarnings("ALL")
+public final class D
+{
+  
+  
+  public static final int iv_social_facebook = R.id.iv_social_facebook;
+  public static final int iv_social_gplus = R.id.iv_social_gplus;
+  public static final int iv_social_twitter = R.id.iv_social_twitter;
+  public static final int iv_social_ytube = R.id.iv_social_ytube;
+
+  public static final int tv_social_facebook = R.id.tv_social_facebook;
+  public static final int tv_social_gplus = R.id.tv_social_gplus;
+  public static final int tv_social_twitter = R.id.tv_social_twitter;
+  public static final int tv_social_ytube = R.id.tv_social_ytube;
+  
+}
